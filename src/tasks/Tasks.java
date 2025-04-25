abce67b4e8b578cef6280775ee8382b6ffe2728e
Gilad <gilad@example.com>
2025-04-26 00:19:07 +0300

    Initial implementation of Tasks class and dependsOn method

--- src/tasks/Tasks.java
+++ src/tasks/Tasks.java
@@ -1,11 +1,37 @@
 package tasks;
 
 public class Tasks {
-private int num;
 
-public Tasks(int num) {
-	this.num=num;
-}
+	private int num; // כמות המשימות
+	private int[][] graph; // מערך של רשימות שכנים (גרף)
+
+	public Tasks(int num) {
+		this.num = num;
+		graph = new int[num][num]; // כאן אתה מאתחל את המערך לפי מספר המשימות
+	}
+
+	boolean dependsOn(int task1, int task2) {
+		
+		if (task1 < 0 || task1 >= num || task2 < 0 || task2 >= num) 
+			return false;
+			
+		graph[task2][task1]=1; //there is an edge between
+		
+		return true;
+			
+			
+		}
+		
+		
+		
+		
+//	public void add() {
+//		int[] tempAmount = new int[totalAmount.length + 1];
+//		for (int i = 0; i < totalAmount.length; i++) {
+//			tempAmount[i] = totalAmount[i];
+//		}
+//		totalAmount = tempAmount;
+//	}
+	}
 
 
-}
