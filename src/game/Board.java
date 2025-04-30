93927df09a98124d7a44196a11ffba8a9948fa79
Gilad <gilad@example.com>
2025-04-30 12:49:11 +0300

    new changes and new method rayLength()

--- src/game/Board.java
+++ src/game/Board.java
@@ -61,12 +61,18 @@ public class Board {
 
 	    return sb.toString();
 	}
+	
+	
+//	protected int maxLineContainin(int i, int j) {
+//		
+//	}
+	
 
 	
 	public static void main(String[] args) {
-		Player p1 = new Player("Bibi", 'B');
-		Player p2 = new Player("Gantz", 'G');
-		Board b = new Board(3,4);
+		Player p1 = new Player("Bibi", 'O');
+		Player p2 = new Player("Gantz", 'X');
+		Board b = new Board(3,3);
 		b.set(0, 0, p1);
 		b.set(1, 0, p1);
 		b.set(2, 2, p2);
@@ -74,6 +80,33 @@ public class Board {
 		b.set(0, 1, p1);
 		System.out.print(b);
 
+ 	}
+	
+	
+	private boolean isInBoard(int i, int j) {
+	    return i >= 0 && i < board.length        // השורה קיימת
+	        && j >= 0 && j < board[0].length;   // העמודה קיימת
+	}
+
+	
+	private int rayLength(int i, int j, int di, int dj) {
+		 if (di == 0 && dj == 0) return 0;
+		 if (!isInBoard(i, j) || isEmpty(i, j)) return 0;
+
+		    
+		    Player current = get(i, j);
+		    int count = 0;
+		    if(isInBoard(i,j)) {
+		    	for()
+		    }
+		    
+		    else {
+		    	System.out.print("not in board");
+		    	return 0;
+		    }
+		  
+		    
+		    
 	}
 
 }
